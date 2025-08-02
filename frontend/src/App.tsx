fd7b17f84aa2b92d01085924ff226208d1a5ecfb
Mridul Khanal <khanalmridul30@gmail.com>
2025-08-02 12:32:16 +0545

    added base CSS with homepage layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,21 @@
+// src/App.tsx
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Forecast from "./pages/Forecast";
 import Navbar from "./components/Navbar";
+import "./index.css";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/forecast" element={<Forecast />} />
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/forecast" element={<Forecast />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
