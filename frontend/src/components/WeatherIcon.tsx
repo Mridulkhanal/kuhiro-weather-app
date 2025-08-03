073c6441fe73177aacd96f92b811fd9a93366ea1
Mridul Khanal <khanalmridul30@gmail.com>
2025-08-04 00:23:01 +0545

    clean, modern, animated weather icon system

--- frontend/src/components/WeatherIcon.tsx
+++ frontend/src/components/WeatherIcon.tsx
@@ -1,20 +1,17 @@
 import React from "react";
 
-// Import SVGs
 import clearIcon from "../assets/weather-icons/clear.svg";
 import cloudyIcon from "../assets/weather-icons/cloudy.svg";
 import rainIcon from "../assets/weather-icons/rain.svg";
 import snowIcon from "../assets/weather-icons/snow.svg";
 
-// Props: condition string (e.g., "Clear", "Clouds", "Rain")
 interface Props {
   condition: string;
 }
 
 const WeatherIcon = ({ condition }: Props) => {
   const lower = condition.toLowerCase();
-
-  let icon = clearIcon; // default
+  let icon = clearIcon;
 
   if (lower.includes("cloud")) icon = cloudyIcon;
   else if (lower.includes("rain")) icon = rainIcon;
@@ -22,7 +19,15 @@ const WeatherIcon = ({ condition }: Props) => {
 
   return (
     <div style={{ textAlign: "center", marginTop: "20px" }}>
-      <img src={icon} alt={condition} style={{ width: "100px", height: "100px" }} />
+      <img
+        src={icon}
+        alt={condition}
+        style={{
+          width: "100px",
+          height: "100px",
+          filter: "drop-shadow(0 0 4px rgba(0,0,0,0.2))",
+        }}
+      />
     </div>
   );
 };
