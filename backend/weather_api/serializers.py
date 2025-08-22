783c755f4058bb6e2ef8a6f456e10d42e1122cb4
Mridul Khanal <khanalmridul30@gmail.com>
2025-08-22 10:26:38 +0545

    added backend for quiz to store and generate questions

--- backend/weather_api/serializers.py
+++ backend/weather_api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Feedback
+from .models import QuizScore
 
 class FeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feedback
         fields = '__all__'
+    
+class QuizScoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuizScore
+        fields = ["id", "player", "level", "score", "created_at"]
--- backend/weather_api/urls.py
+++ backend/weather_api/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('weather/', current_weather),
     path('forecast/', forecast_weather),
     path("feedback/", views.FeedbackView.as_view(), name="feedback"),
+    path("quiz/submit/", views.submit_score, name="submit_score"),
+    path("quiz/leaderboard/", views.leaderboard, name="leaderboard"),
 ]
